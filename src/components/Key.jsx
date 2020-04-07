04e333ed7e62a78996512d0e674fe58c203ad331
kero <kero@comore.bi>
2020-04-07 15:38:03 +0900

    キーボードで演奏可能に

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,33 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import classNames from 'classnames'
 import styles from './Key.module.sass'
 
-const Key = ({ midiKey, type }) => {
+const Key = ({ type, keyMap: [keyCode, midiKey], activeKeys }) => {
   const freq = 440.0 * Math.pow(2.0, (midiKey - 69.0) / 12.0)
 
   const [active, setActive] = useState(false)
   const [oscillator, setOscillator] = useState(false)
-  const buildOscillator = () => {
+
+  const buildOscillator = useCallback(() => {
     const audioContext = new AudioContext()
     const oscillatorNode = audioContext.createOscillator()
     oscillatorNode.type = 'triangle'
     oscillatorNode.frequency.value = freq
     oscillatorNode.connect(audioContext.destination)
     setOscillator(oscillatorNode)
-  }
+  }, [freq])
   useEffect(buildOscillator, [])
 
-  const startSound = () => {
-    if (!active) {
-      oscillator.start()
-      setActive(true)
+  const start = useCallback(() => {
+    if (active) return
+    oscillator.start()
+    setActive(true)
+  }, [active, oscillator])
+  const stop = useCallback(() => {
+    if (!active) return
+    oscillator.stop()
+    setActive(false)
+    buildOscillator()
+  }, [active, oscillator, buildOscillator])
+
+  useEffect(() => {
+    if (activeKeys.includes(keyCode)) {
+      start()
     } else {
-      oscillator.stop()
-      setActive(false)
-      buildOscillator()
+      stop()
     }
-  }
+  }, [activeKeys, keyCode, start, stop])
   return (
-    <div className={styles[type]} onClick={startSound} />
+    <div className={classNames(styles[type], { [styles.active]: active })} onMouseDown={start} onMouseUp={stop} />
   )
 }
 
--- src/components/KeyBoard.jsx
+++ src/components/KeyBoard.jsx
@@ -1,24 +1,53 @@
-import React  from 'react'
+import React, { useState } from 'react'
 import Key from './Key'
 import styles from './KeyBoard.module.sass'
 
 const KeyBoard = () => {
+  const keyMap = {
+    'C4':  [67,  60],
+    'C#4': [70,  61],
+    'D4':  [86,  62],
+    'D#4': [71,  63],
+    'E4':  [66,  64],
+    'F4':  [78,  65],
+    'F#4': [74,  66],
+    'G4':  [77,  67],
+    'G#4': [75,  68],
+    'A4':  [188, 69],
+    'A#4': [76,  70],
+    'B4':  [190, 71],
+    'C5':  [191, 72]
+  }
+  const [activeKeys, setActiveKeys] = useState([])
+  const handleKeyDown = (e) => {
+    if (e.repeat) return
+    setActiveKeys(activeKeys.concat(e.keyCode))
+  }
+  const handleKeyUp = (e) => {
+    setActiveKeys(activeKeys.filter((n) => n !== e.keyCode))
+  }
+  console.log(activeKeys)
   return (
-    <div className={styles.keyboard}>
-      <Key type="white" midiKey={60} />
-      <Key type="white" midiKey={62} />
-      <Key type="white" midiKey={64} />
-      <Key type="white" midiKey={65} />
-      <Key type="white" midiKey={67} />
-      <Key type="white" midiKey={69} />
-      <Key type="white" midiKey={71} />
-      <Key type="white" midiKey={72} />
+    <div
+      className={styles.keyboard}
+      tabIndex={0}
+      onKeyDown={handleKeyDown}
+      onKeyUp={handleKeyUp}
+    >
+      <Key type="white" keyMap={keyMap['C4']} activeKeys={activeKeys} />
+      <Key type="white" keyMap={keyMap['D4']} activeKeys={activeKeys} />
+      <Key type="white" keyMap={keyMap['E4']} activeKeys={activeKeys} />
+      <Key type="white" keyMap={keyMap['F4']} activeKeys={activeKeys} />
+      <Key type="white" keyMap={keyMap['G4']} activeKeys={activeKeys} />
+      <Key type="white" keyMap={keyMap['A4']} activeKeys={activeKeys} />
+      <Key type="white" keyMap={keyMap['B4']} activeKeys={activeKeys} />
+      <Key type="white" keyMap={keyMap['C5']} activeKeys={activeKeys} />
       <div className={styles.black_wrapper}>
-        <Key type="black" midiKey={61} />
-        <Key type="black" midiKey={63} />
-        <Key type="black" midiKey={66} />
-        <Key type="black" midiKey={68} />
-        <Key type="black" midiKey={70} />
+        <Key type="black" keyMap={keyMap['C#4']} activeKeys={activeKeys} />
+        <Key type="black" keyMap={keyMap['D#4']} activeKeys={activeKeys} />
+        <Key type="black" keyMap={keyMap['F#4']} activeKeys={activeKeys} />
+        <Key type="black" keyMap={keyMap['G#4']} activeKeys={activeKeys} />
+        <Key type="black" keyMap={keyMap['A#4']} activeKeys={activeKeys} />
       </div>
     </div>
   )
